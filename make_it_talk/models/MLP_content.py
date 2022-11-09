e13c38f18e5dfac50bf14b9712a7665697eed093
limon8884 <dan.limon.db@gmail.com>
2022-11-09 13:57:37 +0300

    model templates

---
+++ make_it_talk/models/MLP_content.py
@@ -0,0 +1,10 @@
+import torch
+import torch.nn as nn
+
+class MLPContent(nn.Module):
+    def __init__(self) -> None:
+        pass
+
+    def forward(input):
+        return input
+            
---
+++ make_it_talk/models/MLP_speaker.py
@@ -0,0 +1,10 @@
+import torch
+import torch.nn as nn
+
+class MLPSpeaker(nn.Module):
+    def __init__(self) -> None:
+        pass
+
+    def forward(input):
+        return input
+            
