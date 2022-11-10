d6d0c8b74c8937f39f9ee2b58f676706d3e11f52
voidmax <vrom272000@gmail.com>
2022-11-10 11:41:53 +0300

    Add facial_landmarks_extractor

--- make_it_talk/models/facial_landmarks_extractor.py
+++ make_it_talk/models/facial_landmarks_extractor.py
@@ -1,10 +1,14 @@
 import torch
 import torch.nn as nn
+import face_alignment
 
 class FacialLandmarksExtractor(nn.Module):
     def __init__(self) -> None:
         super().__init__()
+        self.model = face_alignment.FaceAlignment(face_alignment.LandmarksType._2D, flip_input=False,  device='cpu')
 
     def forward(self, input):
-        return input
+        return torch.stack([
+            torch.Tensor(self.model.get_landmarks(i)) for i in input
+        ]).view(input.shape[0], -1)
             
