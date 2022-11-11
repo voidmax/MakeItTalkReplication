2e83eafe0acbaa2d0d7401f184ba3021ee1528e3
blunt-octopus <fiveton343@gmail.com>
2022-11-11 14:47:23 +0300

    fixed duplication (this time for real)

--- make_it_talk/scripts/train.py
+++ make_it_talk/scripts/train.py
@@ -4,43 +4,6 @@ import torch.nn.functional as F
 
 from make_it_talk.models import *
 
-def train_pipeline(
-        talking_head_pipeline,
-        generator_optimizer,
-        discriminator_optimizer,
-        train_dataloader,
-        device,
-        generator_loss_function,
-        discriminator_loss_function,
-        metrics_list,
-        training_log=None,
-):
-    if training_log is None:
-        training_log = []
-
-    talking_head_pipeline = talking_head_pipeline.to(device)
-    talking_head_pipeline.train()
-
-    for i, batch in enumerate(train_dataloader):
-
-        training_generator_now = i % 2
-
-        # TODO: find out the format of the batch and change accordingly
-        # whatever is returned should already be torch.Tensors to put to device
-        audios, pictures, true_videos = batch
-
-        audios = audios.to(device)
-        pictures = pictures.to(device)
-        true_videos = true_videos.to(device)
-
-        predicted_landmarks, discriminator_input = talking_head_pipeline.personal_landmarks_predictor(audios, pictures)
-
-        if training_generator_now:
-            loss = generator_loss_function
-        predicted_landmarks, personal_processed, speaker_processed = discriminator_input
-
-        true_landmarks = talking_head_pipeline.facial_landmarks_extractor(true_videos)
-
 
 def train_content_landmarks_predictor(
         talking_head_pipeline: TalkingHeadPipeline,
