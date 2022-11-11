df67ed0fd0451a7df37e722d9241e033226b0222
blunt-octopus <fiveton343@gmail.com>
2022-11-11 15:18:54 +0300

    added gan training pipeline

--- make_it_talk/scripts/train.py
+++ make_it_talk/scripts/train.py
@@ -32,6 +32,7 @@ def train_content_landmarks_predictor(
         results['loss'] = []
         results['metrics'] = []
         for i, batch in enumerate(dataloader):
+
             initial_pictures, audios, true_videos = batch
 
             initial_pictures = initial_pictures.to(device)
@@ -69,6 +70,7 @@ def train_pipeline(
         generator_optimizer,
         discriminator_optimizer,
         train_dataloader,
+        n_epochs,
         device,
         generator_loss_function,
         discriminator_loss_function,
@@ -79,22 +81,64 @@ def train_pipeline(
         training_log = []
 
     talking_head_pipeline = talking_head_pipeline.to(device)
+
+    talking_head_pipeline.audio_to_embedding.requires_grad = False
+    talking_head_pipeline.facial_landmarks_extractor.requires_grad = False
+    talking_head_pipeline.content_landmarks_predictor.requires_grad = False
+
+    talking_head_pipeline.discriminator.requires_grad = True
+    talking_head_pipeline.personal_landmarks_predictor.requires_grad = True
+
     talking_head_pipeline.train()
 
-    for i, batch in enumerate(train_dataloader):
+    for epoch in range(n_epochs):
 
-        training_generator_now = i % 2
+        training_generator_now = epoch % 2
 
-        audios, pictures, true_videos = batch
+        for i, batch in enumerate(train_dataloader):
+            results = {}
 
-        audios = audios.to(device)
-        pictures = pictures.to(device)
-        true_videos = true_videos.to(device)
+            pictures, audios, true_videos = batch
 
-        predicted_landmarks, discriminator_input = talking_head_pipeline.generator(audios, pictures)
+            audios = audios.to(device)
+            pictures = pictures.to(device)
+            true_videos = true_videos.to(device)
 
-        if training_generator_now:
-            loss = generator_loss_function
-        predicted_landmarks, personal_processed, speaker_processed = discriminator_input
+            true_landmarks = talking_head_pipeline.facial_landmarks_extractor(true_videos)
 
-        true_landmarks = talking_head_pipeline.facial_landmarks_extractor(true_videos)
+            predicted_landmarks, discriminator_inputs = talking_head_pipeline(audios, pictures)
+
+            predicted_landmarks, personal_processed, speaker_processed = discriminator_inputs
+            realism_predicted = talking_head_pipeline.discriminator(predicted_landmarks, personal_processed,
+                                                                    speaker_processed)
+            realism_true = talking_head_pipeline.discriminator(true_landmarks, personal_processed, speaker_processed)
+
+            if training_generator_now:
+                loss = generator_loss_function(predicted_landmarks, realism_predicted, true_landmarks)
+                results['generator_loss'] = loss.item()
+                results['discriminator_loss'] = None
+                generator_optimizer.zero_grad()
+                loss.backward()
+                generator_optimizer.step()
+            else:
+                predicted_landmarks, personal_processed, speaker_processed = discriminator_inputs
+                realism_predicted = talking_head_pipeline.discriminator(predicted_landmarks, personal_processed, speaker_processed)
+                realism_true = talking_head_pipeline.discriminator(true_landmarks, personal_processed, speaker_processed)
+
+                loss = discriminator_loss_function(realism_predicted, realism_true)
+                results['generator_loss'] = None
+                results['discriminator_loss'] = loss.item()
+                discriminator_optimizer.zero_grad()
+                loss.backward()
+                discriminator_optimizer.step()
+
+            metric_results = []
+            for metric in metrics_list:
+                metric_results.append(metric(predicted_landmarks, true_landmarks))
+            results['metrics'] = metric_results
+
+        training_log.append(results)
+
+    return training_log
+
+
