46e4835ec9c3a09cc20aebfb766e18dd2f3587ee
limon8884 <dan.limon.db@gmail.com>
2022-11-11 18:31:45 +0300

    self attention

--- make_it_talk/models/self_attention_encoder.py
+++ make_it_talk/models/self_attention_encoder.py
@@ -1,6 +1,28 @@
 import torch
 import torch.nn as nn
 
+class SelfAttentionEncoder(nn.Module):
+    def __inti__(self, hidden_size_1, hidden_size_2, hidden_size_4, nhead=2, dim_feedforward=200, dropout=0.2):
+        super().__init__()
+        d_model = hidden_size_1 + hidden_size_2
+        self.attn = nn.SelfAttentionEncoder(
+            d_model, 
+            nhead, 
+            dim_feedforward, 
+            dropout,
+            batch_first=True, 
+        )
+        self.linear = nn.Linear(d_model, hidden_size_4)
+
+    def forward(self, x_spk, x_cont):
+        t = x_cont.shape[1]
+        x = torch.cat([x_cont, x_spk.unsqueeze(1).repeat(1, t, 1)], dim=-1)
+        x = self.attn(x)
+        x = x[:, 0, ...].squeeze(1)
+        x = self.linear(x)
+        return x
+
+
 class SelfAttentionEncoderPlug(nn.Module):
     def __init__(self, in_hs, out_hs):
         super(SelfAttentionEncoderPlug, self).__init__()
