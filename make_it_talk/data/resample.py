a9b279f63a2b5b3920148ff7ae8dee128e9ef06f
limon8884 <dan.limon.db@gmail.com>
2022-11-15 13:26:57 +0300

    resampler

---
+++ make_it_talk/data/resample.py
@@ -0,0 +1,14 @@
+from pydub import AudioSegment
+from pathlib import Path
+
+target_freq = 16000
+
+def resample_file(path_from, path_to):
+    sound = AudioSegment.from_wav(path_from)
+    sound_w_new_fs = sound.set_frame_rate(target_freq)
+    sound_w_new_fs.export(path_to, format="wav")
+
+def resample(input_dir_path):
+    paths = Path(input_dir_path).glob('**/*.m4a')
+    for path in paths:
+        resample_file(path, path[:-3] + 'wav')
